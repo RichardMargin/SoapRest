5b5c5f50f2a6a7fdbde7326cb3dfaa75bee0a5a8
rehioui walid <walid.rehioui@telecomnancy.eu>
2023-01-18 11:42:50 +0100

    Ajout premiers Tus

--- src/test/java/com/hopital/exorestsoap/controller/MedecinControllerTest.java
+++ src/test/java/com/hopital/exorestsoap/controller/MedecinControllerTest.java
@@ -1,8 +1,63 @@
 package com.hopital.exorestsoap.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.hopital.exorestsoap.controllers.MedecinController;
+import com.hopital.exorestsoap.models.Medecin;
+import com.hopital.exorestsoap.services.MedecinService;
 
 @RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class MedecinControllerTest {
+	
+	@InjectMocks
+	private MedecinController medecinController;
+	
+	@Mock
+	private MedecinService medecinService;
+	
+	@Test
+	public void getMedecinAll_NotEmptyTest() throws Exception{
+		//Given
+		List<Medecin> medecinList = new ArrayList<>();
+		
+		Medecin medecinMock1 = new Medecin();
+		Medecin medecinMock2 = new Medecin();
+		
+		medecinList.add(medecinMock1);
+		medecinList.add(medecinMock2);
+		
+		//When
+		Mockito.when(medecinService.findAll()).thenReturn(medecinList);
+		ResponseEntity<List<Medecin>> response = medecinController.findAll();
+		
+		//Then
+		assertEquals(2,response.getBody().size());
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+	}
+	
+	@Test
+	public void getMedecinAll_EmptyTest() throws Exception{
+		//Given
+		List<Medecin> medecinList = new ArrayList<>();
+	    
+		//When
+		Mockito.when(medecinService.findAll()).thenReturn(medecinList);
+		ResponseEntity<List<Medecin>> response = medecinController.findAll();
+		
+		//Then
+		assertEquals(null,response.getBody());
+		assertEquals(HttpStatus.NO_CONTENT,response.getStatusCode());
+	}
 
 }
--- src/test/java/com/hopital/exorestsoap/controller/PatientControllerTest.java
+++ src/test/java/com/hopital/exorestsoap/controller/PatientControllerTest.java
@@ -1,8 +1,62 @@
 package com.hopital.exorestsoap.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.hopital.exorestsoap.controllers.PatientController;
+import com.hopital.exorestsoap.models.Patient;
+import com.hopital.exorestsoap.services.PatientService;
 
 @RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class PatientControllerTest {
+	@InjectMocks
+	private PatientController patientController;
+	
+	@Mock
+	private PatientService patientService;
+	
+	@Test
+	public void getPatientAll_NotEmptyTest() throws Exception{
+		//Given
+		List<Patient> patientList = new ArrayList<>();
+		
+		Patient patientMock1 = new Patient();
+		Patient patientMock2 = new Patient();
+		
+		patientList.add(patientMock1);
+		patientList.add(patientMock2);
+		
+		//When
+		Mockito.when(patientService.findAll()).thenReturn(patientList);
+		ResponseEntity<List<Patient>> response = patientController.findAll();
+		
+		//Then
+		assertEquals(2,response.getBody().size());
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+	}
+	
+	@Test
+	public void getPatientAll_EmptyTest() throws Exception{
+		//Given
+		List<Patient> medecinList = new ArrayList<>();
+	    
+		//When
+		Mockito.when(patientService.findAll()).thenReturn(medecinList);
+		ResponseEntity<List<Patient>> response = patientController.findAll();
+		
+		//Then
+		assertEquals(null,response.getBody());
+		assertEquals(HttpStatus.NO_CONTENT,response.getStatusCode());
+	}
 
 }
