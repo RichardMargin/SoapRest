12de22b11e76cede11c66e2604cc393bd8c897cc
rehioui walid <walid.rehioui@telecomnancy.eu>
2023-01-18 12:20:56 +0100

    ajouts de quelques tus

--- src/test/java/com/hopital/exorestsoap/controller/MedecinControllerTest.java
+++ src/test/java/com/hopital/exorestsoap/controller/MedecinControllerTest.java
@@ -1,9 +1,12 @@
 package com.hopital.exorestsoap.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -58,6 +61,43 @@ public class MedecinControllerTest {
 		//Then
 		assertEquals(null,response.getBody());
 		assertEquals(HttpStatus.NO_CONTENT,response.getStatusCode());
+	}
+	
+	@Test
+	public void findMedecinById_NotEmpty_Test() throws Exception{
+		//Given
+		Medecin medecinMock = new Medecin();
+		medecinMock.setId(1L);
+		medecinMock.setNom("Dupont");
+		medecinMock.setPrenom("Jean-Christophe");
+		medecinMock.setRdvs(null);
+		
+		Optional<Medecin> optionalMock = Optional.of(medecinMock);
+		
+		//When
+		Mockito.when(medecinService.findById(Mockito.any())).thenReturn(optionalMock);
+		ResponseEntity<Medecin> response = medecinController.findById(1L);
+		
+		//Then
+		assertThat(response.getBody().getId()).isEqualTo(1L);
+		assertThat(response.getBody().getPrenom()).isEqualTo("Jean-Christophe");
+		assertThat(response.getBody().getNom()).isEqualTo("Dupont");
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+	
+	
+	@Test
+	public void findMedecinById_Empty_Test() throws Exception{
+		//Given
+
+		
+		//When
+		Mockito.when(medecinService.findById(Mockito.any())).thenReturn(Optional.empty());
+		ResponseEntity<Medecin> response = medecinController.findById(1L);
+		
+		//Then
+		assertThat(response.getBody()).isEqualTo(null);
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
 	}
 
 }
--- src/test/java/com/hopital/exorestsoap/controller/PatientControllerTest.java
+++ src/test/java/com/hopital/exorestsoap/controller/PatientControllerTest.java
@@ -1,9 +1,11 @@
 package com.hopital.exorestsoap.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.hopital.exorestsoap.controllers.PatientController;
+import com.hopital.exorestsoap.models.Medecin;
 import com.hopital.exorestsoap.models.Patient;
 import com.hopital.exorestsoap.services.PatientService;
 
@@ -57,6 +60,44 @@ public class PatientControllerTest {
 		//Then
 		assertEquals(null,response.getBody());
 		assertEquals(HttpStatus.NO_CONTENT,response.getStatusCode());
+	}
+	
+	
+	@Test
+	public void findPatientById_NotEmpty_Test() throws Exception{
+		//Given
+		Patient patientMock = new Patient();
+		patientMock.setId(1L);
+		patientMock.setNom("Dupont");
+		patientMock.setPrenom("Jean-Christophe");
+		patientMock.setRdvs(null);
+		
+		Optional<Patient> optionalMock = Optional.of(patientMock);
+		
+		//When
+		Mockito.when(patientService.findById(Mockito.any())).thenReturn(optionalMock);
+		ResponseEntity<Patient> response = patientController.findById(1L);
+		
+		//Then
+		assertThat(response.getBody().getId()).isEqualTo(1L);
+		assertThat(response.getBody().getPrenom()).isEqualTo("Jean-Christophe");
+		assertThat(response.getBody().getNom()).isEqualTo("Dupont");
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+	
+	
+	@Test
+	public void findPatientById_Empty_Test() throws Exception{
+		//Given
+
+		
+		//When
+		Mockito.when(patientService.findById(Mockito.any())).thenReturn(Optional.empty());
+		ResponseEntity<Patient> response = patientController.findById(1L);
+		
+		//Then
+		assertThat(response.getBody()).isEqualTo(null);
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
 	}
 
 }
